9a624460ffec98223d174ebb3cf537fe6a5c3062
Luther A <106061769+lua0repo@users.noreply.github.com>
2025-02-12 03:36:26 +0800

    Create CalculatorServiceImpl.java

---
+++ Simple Calculator Service/CalculatorServiceImpl.java
@@ -0,0 +1,28 @@
+import javax.jws.WebService;
+
+@WebService(endpointInterface = "CalculatorService")
+public class CalculatorServiceImpl implements CalculatorService {
+
+    @Override
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    @Override
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    @Override
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    @Override
+    public int divide(int a, int b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+}
